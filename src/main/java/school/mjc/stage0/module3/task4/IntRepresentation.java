22dfbf00a4c8618bc4b258d3771540434d864603
Endorsmoch <a16003872@alumnos.uady.mx>
2023-09-27 10:12:29 -0600

    Finished

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c = 'c', h = 'h', a = 'a', r = 'r';
+        System.out.println((int) c);
+        System.out.println((int) h);
+        System.out.println((int) r);
     }
 }
